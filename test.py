9e0cb5f8ea63bb2429a3e2899a4e26e78cd6a13a
Max Müller <maxmullerfitness@gmail.com>
2023-03-10 17:14:28 -0300

    update

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-import pygame, random, time, sys
+import pygame
+import random
+import time
+import sys
 
 # Inicialize o Pygame
 pygame.init()
@@ -75,13 +78,14 @@ while True:
         if died:
             window.blit(bird_dead,(100,500))
         window.blit(font.render('IMS Bird', True, (0,0,0), None),(10,0))
-        window.blit(caption,(10,620))
+        window.blit(caption,(150,620))
         window.blit(font2.render('Pontuação mais alta - ' + str(hscore), True, (0,0,0), None),(10,70))
 
     # Verifique se o pássaro atingiu um obstáculo ou a borda da tela
-   if ypos >= 528 or ((pipe[0] < 164 and pipe[0] > 14) and (ypos+192 > pipe[1]+300 or ypos < pipe[1])):
-    ypos, start, tscore, pipe[0], died, caption = 528, False, 0, 720, True, font2.render('Você perdeu', True, (0,0,0), None)
-    if died:
-        time.sleep(2) # espera 2 segundos
-        game_state = 0 # redefine o estado do jogo para "não começou"
-        died = False # redefine a variável "died" para falso
+    if ypos >= 528 or ((pipe[0] < 164 and pipe[0] > 14) and (ypos+192 > pipe[1]+300 or ypos < pipe[1])):
+        ypos, start, tscore, pipe[0], died, caption = 528, False, 0, 720, True, font2.render('Você perdeu', True, (0,0,0), None)
+        game_state = 0
+    elif ypos < 0:
+        ypos, vel = 0, -abs(vel)
+    clock.tick(60)
+    pygame.display.flip()
